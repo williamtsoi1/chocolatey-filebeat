e26f971a87ea97a74816b7aea3e608461b54526e
William Tsoi <william@williamtsoi.net>
2016-05-25 00:58:33 +0000

    initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,45 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'FileBeat'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://download.elastic.co/beats/filebeat/filebeat-1.2.3-windows.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE_MSI_OR_MSU'
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'FileBeat*'
+  checksum      = ''
+  checksumType  = 'md5'
+  checksum64    = ''
+  checksumType64= 'md5'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Invoke-Expression $(Join-Path $toolsDir "filebeat-1.2.3-windows\install-service-filebeat.ps1")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Invoke-Expression $(Join-Path $toolsDir "filebeat-1.2.3-windows\uninstall-service-filebeat.ps1")
+Uninstall-ChocolateyZipPackage
+
+
